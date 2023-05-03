ce3a1a61803837537c5247a19288c26e8d13d1e8
juanjo1998 <juanjosr98@gmail.com>
2023-05-03 11:59:05 -0500

    menu-style completed

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express()
 
 // hbs
 app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + "/views/partials")
+
+// ? static content
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('home')
