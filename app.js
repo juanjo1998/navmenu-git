2835277f64185ca70fdb3eacdd05a124afee4640
juanjo1998 <juanjosr98@gmail.com>
2023-05-03 14:11:22 -0500

    example

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 const express = require('express')
 const hbs = require('hbs')
 const port = 8080
+const example = "example"
 
 // express app
 const app = express()
@@ -18,6 +18,3 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => console.log(`Server listening on port ${port}`))
-=======
-h2llo
->>>>>>> j2dev
