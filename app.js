7e990b02d4475f43b7f08d29f109bbd252f3edbd
juanjo1998 <juanjosr98@gmail.com>
2023-05-03 14:09:04 -0500

    0

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const hbs = require('hbs')
+const port = 8080
+
+// express app
+const app = express()
+
+// hbs
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + "/views/partials")
+
+// ? static content
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => console.log(`Server listening on port ${port}`))
